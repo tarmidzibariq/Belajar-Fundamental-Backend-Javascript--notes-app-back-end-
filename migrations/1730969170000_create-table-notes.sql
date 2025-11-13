2956638696bbd6da8a2252dcf3bc621592a872e2
Tarmidzi Bariq <81232207+tarmidzibariq@users.noreply.github.com>
2025-11-14 05:46:57 +0700

    implement user management with PostgreSQL, including user creation and validation

--- migrations/1730969170000_create-table-notes.sql
+++
@@ -1,9 +0,0 @@
--- Create table notes
-CREATE TABLE notes (
-    id VARCHAR(50) PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    body TEXT,
-    tags TEXT[],
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
---
+++ migrations/1762964713255_create-table-users.js
@@ -0,0 +1,30 @@
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+    pgm.createTable('users', {
+        id : {
+            type : 'VARCHAR(50)',
+            primaryKey : true,
+        },
+        username: {
+            type : 'VARCHAR(50)',
+            unique : true,
+            notNull: true,
+        },
+        password: {
+            type: 'TEXT',
+            notNull : true,
+        },
+        fullName: {
+            type: 'TEXT',
+            notNull: true,
+        }
+    });
+};
+
+exports.down = (pgm) => {
+    pgm.dropTable('users');
+};
+
+
