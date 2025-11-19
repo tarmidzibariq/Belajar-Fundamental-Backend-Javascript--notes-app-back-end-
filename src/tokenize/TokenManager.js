29babbf8d39eabac8caec91ea8c31b06c49bb1f2
Tarmidzi Bariq <81232207+tarmidzibariq@users.noreply.github.com>
2025-11-19 19:24:23 +0700

    feat: implement authentication module with route registration and TokenManager refactor

--- src/tokenize/TokenManager.js
+++ src/tokenize/TokenManager.js
@@ -1,7 +1,7 @@
 const Jwt = require('@hapi/jwt');
 const InvariantError = require('../exceptions/InvariantError');
 
-const tokenManager = {
+const TokenManager = {
     generateAccessToken : (payload) => Jwt.token.generate(payload, process.env.ACCESS_TOKEN_KEY),
     generateRefreshToken : (payload) => Jwt.token.generate(payload, process.env.REFRESH_TOKEN_KEY),
     verifyRefreshToken : (refreshToken) => {
@@ -18,4 +18,4 @@ const tokenManager = {
     },
 };
 
-module.exports = tokenManager;
+module.exports = TokenManager;
