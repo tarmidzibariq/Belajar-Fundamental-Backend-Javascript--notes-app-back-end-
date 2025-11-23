714d1115fe83871355afc1434a059201cbb05d1d
Tarmidzi Bariq <81232207+tarmidzibariq@users.noreply.github.com>
2025-11-24 05:40:13 +0700

    feat: implement note ownership verification and update note service methods

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -15,14 +15,14 @@ class NotesHandler {
   async postNoteHandler(request, h) {
     try {
       this._validator.validateNotePayload(request.payload);
-      const {
-        title = 'untitled', body, tags,
-      } = request.payload;
+      const {title = 'untitled', body, tags,} = request.payload;
+      const { id:credentialId } = request.auth.credentials
 
       const noteId = await this._service.addNote({
         title,
         body,
         tags,
+        owner: credentialId
       });
 
       const response = h.response({
@@ -56,8 +56,9 @@ class NotesHandler {
     }
   }
 
-  async getNotesHandler() {
-    const notes = await this._service.getNotes();
+  async getNotesHandler(request) {
+    const { id: credentialId } = request.auth.credentials;
+    const notes = await this._service.getNotes(credentialId);
     return {
       status: 'success',
       data: {
@@ -68,9 +69,10 @@ class NotesHandler {
 
   async getNoteByIdHandler(request, h) {
     try {
-      const {
-        id,
-      } = request.params;
+      const { id } = request.params;
+      const { id: credentialId } = request.auth.credentials;
+
+      await this._service.verifyNoteOwner(id, credentialId);
       const note = await this._service.getNoteById(id);
       return {
         status: 'success',
@@ -103,9 +105,10 @@ class NotesHandler {
   async putNoteByIdHandler(request, h) {
     try {
       this._validator.validateNotePayload(request.payload);
-      const {
-        id,
-      } = request.params;
+      const { id } = request.params;
+      const { id: credentialId } = request.auth.credentials;
+      
+      await this._service.verifyNoteOwner(id, credentialId);
 
       await this._service.editNoteById(id, request.payload);
 
@@ -137,9 +140,10 @@ class NotesHandler {
 
   async deleteNoteByIdHandler(request, h) {
     try {
-      const {
-        id,
-      } = request.params;
+      const { id } = request.params;
+      const { id: credentialId} = request.auth.credentials;
+
+      await this._service.verifyNoteOwner(id, credentialId);
       await this._service.deleteNoteById(id);
 
       return {
--- src/services/postgres/NotesService.js
+++ src/services/postgres/NotesService.js
@@ -3,20 +3,21 @@ const { Pool } = require('pg');
 const InvariantError = require('../../exceptions/InvariantError');
 const NotFoundError = require('../../exceptions/NotFoundError');
 const { mapDBToModel } = require('../../utils');
+const AuthorizationError = require('../../exceptions/AuthorizationError');
 
 class NotesService {
   constructor() {
     this._pool = new Pool();
   }
 
-  async addNote({ title, body, tags }) {
+  async addNote({ title, body, tags, owner }) {
     const id = nanoid(16);
     const createdAt = new Date().toISOString();
     const updatedAt = createdAt;
 
     const query = {
-      text: 'INSERT INTO notes VALUES( $1, $2, $3, $4, $5, $6 ) RETURNING id',
-      values: [id, title, body, tags, createdAt, updatedAt],
+      text: 'INSERT INTO notes VALUES( $1, $2, $3, $4, $5, $6, $7 ) RETURNING id',
+      values: [id, title, body, tags, createdAt, updatedAt, owner],
     };
 
     const result = await this._pool.query(query);
@@ -46,10 +47,14 @@ class NotesService {
     // return id;
   }
 
-  async getNotes() {
-    const result = await this._pool.query('SELECT * FROM notes');
-    return result.rows.map(mapDBToModel);
-  }
+  async getNotes(owner) {
+  const query = {
+    text: 'SELECT * FROM notes WHERE owner = $1',
+    values: [owner],
+  };
+  const result = await this._pool.query(query);
+  return result.rows.map(mapDBToModel);
+}
 
   async getNoteById(id) {
     const query = {
@@ -116,6 +121,24 @@ class NotesService {
     // }
 
     // this._notes.splice(index, 1);
+  }
+  async verifyNoteOwner(id, owner) {
+    const query ={
+      text: 'SELECT * FROM notes WHERE id = $1',
+      values : [id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if(!result.rows.length) {
+      throw new NotFoundError('Catatan tidak ditemukan');
+    }
+
+    const note = result.rows[0];
+
+    if (note.owner !== owner) {
+      throw new AuthorizationError('Anda tidak berhak mengakses resource ini');
+    }
   }
 }
 
