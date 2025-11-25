d29aec134684d0445768799f7e5b38572c2d3cde
Tarmidzi Bariq <81232207+tarmidzibariq@users.noreply.github.com>
2025-11-25 09:02:11 +0700

    feat: implement delete and verify collaboration methods in CollaborationsService

--- src/services/postgres/CollaborationsService.js
+++ src/services/postgres/CollaborationsService.js
@@ -23,4 +23,32 @@ class CollaborationsService {
 
     return result.rows[0].id;
   }
+
+  async deleteCollaboration(noteId, userId) {
+    const query = {
+      text: 'DELETE FROM collaborations WHERE note_id = $1 AND user_id = $2 RETURNING id',
+      values: [noteId, userId],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new InvariantError('Kolaborasi gagal dihapus');
+    }
+  }
+
+  async verifyCollaborator(noteId, userId) {
+    const query = {
+      text: 'SELECT * FROM collaborations WHERE note_id = $1 AND user_id = $2',
+      values: [noteId, userId],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new InvariantError('Kolaborasi gagal diverifikasi');
+    }
+  }
 }
+
+module.exports = CollaborationsService;
