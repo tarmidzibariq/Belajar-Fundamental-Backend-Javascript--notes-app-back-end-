3f1410021a6206ad1e310366c9a8da3980ca8576
Tarmidzi Bariq <81232207+tarmidzibariq@users.noreply.github.com>
2025-11-27 19:56:53 +0700

    refactor code

--- src/validator/collaborations/schema.js
+++ src/validator/collaborations/schema.js
@@ -1,10 +1,10 @@
-const Joi = require("joi");
+const Joi = require('joi');
 
 const CollaborationPayloadSchema = Joi.object({
-    noteId: Joi.string().required(),
-    userId: Joi.string().required(),
+  noteId: Joi.string().required(),
+  userId: Joi.string().required(),
 });
 
 module.exports = {
-    CollaborationPayloadSchema
+  CollaborationPayloadSchema,
 };
