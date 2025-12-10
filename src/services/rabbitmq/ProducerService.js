235687e25d417907f79eea9ef9e55e66c566db63
Tarmidzi Bariq <81232207+tarmidzibariq@users.noreply.github.com>
2025-12-10 10:21:19 +0700

    Add amqplib dependency and implement ProducerService for RabbitMQ messaging

---
+++ src/services/rabbitmq/ProducerService.js
@@ -0,0 +1,17 @@
+const amqp = requeire('amqplib');
+const ProducerService = {
+    sendMessage: async (queue, message) => {
+        const connection = await amqp.connect(process.env.RABBITMQ_SERVER);
+        const channel = await connection.createChannel();
+        await channel.assertQueue(queue, { 
+            durable: true 
+        });
+
+        await channel.sendToQueue(queue, Buffer.from(message));
+        setTimeout(() => {
+            connection.close();
+        }, 1000);
+    },
+};
+
+module.exports = ProducerService;
